89cfb8e2d5d6a42e9ff84948e0e3e57bbc712429
unknown <s1910537@u.tsukuba.ac.jp>
2019-06-12 20:20:01 +0900

    [update] create graph

--- plot.py
+++ plot.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 kabu11A = pd.read_csv('./data/kabu_1_1_A.csv')
 kajo11A = pd.read_csv('./data/kajo_1_1_A.csv')
 
-print(kabu11A['X'])
+print('個体数' + str(len(kabu11A['X'])))
 
 X = kabu11A['X']
 Y = kabu11A['Y']
-plt.scatter(X, Y)
+plt.scatter(X, Y, marker = "+", color = "red")
 
 X = kajo11A['X']
 Y = kajo11A['Y']
-plt.scatter(X, Y, marker = "+")
+plt.scatter(X, Y, marker = ".", color = "orange", s = 5)
+
+plt.xlim(0,500)
+plt.ylim(0,500)
+plt.axes().set_aspect('equal')
+plt.title('調査区1-1 PlantA')
+plt.xlabel('X軸[cm]')
+plt.ylabel('Y軸[cm]')
 plt.show()
