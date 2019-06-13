283a2a87c27436ee1244d87f6bb0ed88bd01e523
unknown <s1910537@u.tsukuba.ac.jp>
2019-06-13 10:33:33 +0900

    [update] create loop

--- plot.py
+++ plot.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-kabu11A = pd.read_csv('./data/kabu_1_1_A.csv')
-kajo11A = pd.read_csv('./data/kajo_1_1_A.csv')
+for i in range(0, 8):
+    group = i // 2 + 1
 
-print('個体数' + str(len(kabu11A['X'])))
+    mod = i % 2
+    if mod != 0:
+        team = 2
+    else:
+        team = 1
 
-X = kabu11A['X']
-Y = kabu11A['Y']
-plt.scatter(X, Y, marker = "+", color = "red")
+    kabuA = pd.read_csv('./data/kabu_' + str(group) + '_' + str(team) + '_A.csv')
+    kajoA = pd.read_csv('./data/kajo_' + str(group) + '_' + str(team) + '_A.csv')
 
-X = kajo11A['X']
-Y = kajo11A['Y']
-plt.scatter(X, Y, marker = ".", color = "orange", s = 5)
+    print(str(group) + '-' + str(team) + ' PlantA 個体数' + str(len(kabuA['X'])))
 
-plt.xlim(0,500)
-plt.ylim(0,500)
-plt.axes().set_aspect('equal')
-plt.title('調査区1-1 PlantA')
-plt.xlabel('X軸[cm]')
-plt.ylabel('Y軸[cm]')
-plt.show()
+    X = kabuA['X']
+    Y = kabuA['Y']
+    plt.scatter(X, Y, marker = "+", color = "red")
+
+    X = kajoA['X']
+    Y = kajoA['Y']
+    plt.scatter(X, Y, marker = ".", color = "orange", s = 5)
+
+    plt.xlim(0,500)
+    plt.ylim(0,500)
+    plt.axes().set_aspect('equal')
+    plt.title('Area' + str(group) + '-' + str(team) + ' PlantA')
+    plt.xlabel('X axis[cm]')
+    plt.ylabel('Y axis[cm]')
+    plt.show()
